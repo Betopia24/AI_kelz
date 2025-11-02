409975567e564fb82c43ee8255f827231d3bd9e3
dumb-meh <omorfaruk549@gmail.com>
2025-11-02 17:01:58 +0600

    removed unnecessary files

--- app/services/deviation/attachement_new/attachement_new_schema.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class AttachementRequest(BaseModel):
-    user_input:str
-    exsting_file_titles:List [str]
-
-class AttachementResponse (BaseModel):
-    new_file_titles:List[str]
