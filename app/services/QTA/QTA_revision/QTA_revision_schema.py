3f25e88c39719ed453b3fed8b80351cd501b723e
dumb-meh <omorfaruk549@gmail.com>
2025-11-04 10:30:13 +0600

    minor changes

--- app/services/QTA/QTA_revision/QTA_revision_schema.py
+++ app/services/QTA/QTA_revision/QTA_revision_schema.py
@@ -17,12 +17,12 @@ class final_qta_revision_request(BaseModel):
 
 class final_qta_revision_response(BaseModel):
     action_summary:str
-    change_details:Dict[str,Any]
+    change_details:str
     new_document_text:str
     
 class repeat_qta_revision_request(BaseModel):
     transcribed_text: str
-    existing_change_details: Dict[str, Any]
+    existing_change_details: str
     existing_action_summary: str
     existing_document: str
     
